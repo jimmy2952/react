8ce7713de77a8c09e183d182c1d47ec1a191d27b
jimmy2952 <jimmy2952@gmail.com>
2020-08-17 14:06:56 +0800

    complete-app

--- job-board/src/App.js
+++ job-board/src/App.js
@@ -4,23 +4,37 @@ import './App.css';
 class App extends Component{
   constructor(props) {
     super(props)
+    this.state = {
+      jobs: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://www.mocky.io/v2/5c18f4ac2f00002a00af130a')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          jobs: data
+        })
+      })
   }
 
   render() {
+    const {jobs} = this.state
     return (
       <div className="App">
         <h1>Job board 職缺報報</h1>
         <div className='jobs'>
-          <div className='job'>
-            <h2>前端工程師</h2>
-            <p>前端工程師前端工程師前端工程師前端工程師</p>
-            <a href='https://google.com'>我要應徵</a>
-          </div>
-          <div className='job'>
-            <h2>前端工程師</h2>
-            <p>前端工程師前端工程師前端工程師前端工程師</p>
-            <a href='https://google.com'>我要應徵</a>
-          </div>
+          {
+            jobs.map(job => (
+              <div className='job'>
+                <h2>{job.title}</h2>
+                <p className='job__content'>{job.content}</p>
+                <p>薪水範圍：{job.salary}</p>
+                <a href={job.link} target='_blank'>我要應徵</a>
+              </div>
+            ))
+          }
         </div>
       </div>
     )
